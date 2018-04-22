79a2b8ff483425730c6db7234071a39eb668f2c6
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-22 23:34:57 +0200

    actual code & actual tests

--- dmdm/mail.py
+++ dmdm/mail.py
@@ -1,11 +1,20 @@
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives, get_connection
+from django.template.loader import get_template
 
 from mdmail.api import EmailContent
 
 
-class MarkdownMailMessage(EmailMessage):
-    pass
+def send_mail(subject, message, from_email, recipient_list, context=None, request=None, fail_silently=False,
+              css=None, image_root='.', auth_user=None, auth_password=None, connection=None):
+    connection = connection or get_connection(
+        username=auth_user,
+        password=auth_password,
+        fail_silently=fail_silently,
+    )
+    if context is not None:
+        message = get_template(message).render(context, request)
+    content = EmailContent(message, css=css, image_root=image_root)
+    mail = EmailMultiAlternatives(subject, content.text, from_email, recipient_list, connection=connection)
+    mail.attach_alternative(content.html, 'text/html')
 
-
-class DjangoEmailContent(EmailContent):
-    pass
+    return mail.send()
