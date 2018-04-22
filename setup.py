468b1694adfb2658cd19dce59b3d4390d67b763d
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-04-22 23:56:30 +0200

    version 1.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dmdm',
-    version='1.0.0',
+    version='1.0.1',
     packages=['dmdm'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
