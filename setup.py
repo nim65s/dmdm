a3a9c245a4b8db5543827919f988bae9bdcee680
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-08-05 00:23:06 +0200

    use Pipfile

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
+import os
+import re
+from subprocess import check_output
+
 from setuptools import setup
 
-with open('README.md') as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-with open('requirements.in') as requirements:
-    REQUIREMENTS = requirements.readlines()
+with open(os.path.join(os.path.dirname(__file__), 'Pipfile')) as pipfile:
+    content = pipfile.read()
+    REQUIREMENTS = re.findall('''\\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
+    PYTHON_VERSION = re.search('''python_version *= *['"]*([\d.]+)['"]?''', content)[1]
+
+VERSION = [tag for tag in check_output(['git', 'tag', '-l']).decode().split() if tag.startswith('v')][-1][1:]
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dmdm',
-    version='1.0.2',
+    version=VERSION,
     packages=['dmdm'],
     install_requires=REQUIREMENTS,
     include_package_data=True,
@@ -21,7 +32,7 @@ setup(
     url='https://github.com/nim65s/dmdm',
     author='Guilhem Saurel',
     author_email='webmaster@saurel.me',
-    python_requires='>=3',
+    python_requires=f'>={PYTHON_VERSION}',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -29,7 +40,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
