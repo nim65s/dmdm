f03cc1e17891169c16f76558fdda5e59056091b8
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-09-16 10:11:37 +0200

    use raw strings for regex

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 with open(os.path.join(os.path.dirname(__file__), 'Pipfile')) as pipfile:
     content = pipfile.read()
-    REQUIREMENTS = re.findall('''\\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
-    PYTHON_VERSION = re.search('''python_version *= *['"]*([\d.]+)['"]?''', content)[1]
+    REQUIREMENTS = re.findall(r'''\\n *['"]?([\w-]*)['"]? *=''', content.split('packages]')[1])
 
 VERSION = [tag for tag in check_output(['git', 'tag', '-l']).decode().split() if tag.startswith('v')][-1][1:]
 
@@ -40,7 +39,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
